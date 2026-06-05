f4a7ea8c279b27c379c3b14cb5beda31af62305f
umar <42.musajid@gmail.com>
2026-06-05 15:05:58 +0300

    add the queries and handlefunctions for creating users and reseting database

--- main.go
+++ main.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"database/sql"
-	//"encoding/json"
+	"encoding/json"
 	"os"
-	//"strings"
 	"log"
 	"net/http"
 	"sync/atomic"
-
-	//"sync/atomic"
 	"fmt"
+	"time"
 
-	//"github.com/joho/godotenv"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-
 	"github.com/umarsajidm/birdie/internal/database"
 )
+
+type User struct {
+	ID	uuid.UUID `json:"id"`
+	CreatedAt time.Time	`json:"created_at"`
+	UpdatedAt	time.Time	`json:"updated_at"`
+	Email	string	`json:"email"`
+}
+
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	DB             *database.Queries
+	Platform		string
 }
 
 // middleware for statefull handler
@@ -42,11 +48,57 @@ func (cfg *apiConfig) MetricsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
 }
 
-// middleware Reset
+//ResetHandler
 func (cfg *apiConfig) ResetHandler(w http.ResponseWriter, r *http.Request) {
 
+	if cfg.Platform != "dev" {
+		respondWithERROR(w, http.StatusForbidden, "reset is only allowed in dev environment")
+		return
+	}
+
+	//wiping the fileserver hits
 	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
+	//executing the sqlc delete query
+	err := cfg.DB.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithERROR(w, http.StatusInternalServerError, "couldnt delete users")
+		return
+	}
+	respondWithValid(w, http.StatusOK, struct {
+
+		Message string `json:"message"`
+	}{
+		Message: "hits reset and database wiped cleanly",
+	})
+}
+
+//handleUsersCreate
+func (cfg *apiConfig) handleUsersCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	//decoding the incoming json body/ request
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithERROR(w, http.StatusBadRequest, "couldn't decode the params for creating users")
+		return
+	}
+	//executing the sqlc query
+	//passing the r.Context() to handle the request timeouts natively
+	user, err := cfg.DB.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		respondWithERROR(w, http.StatusBadRequest, "couldnt create user")
+		return
+	}
+	//map the database.User to my custom User struct and send the json response
+	respondWithValid(w, http.StatusCreated, User{
+		ID:	user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:	user.Email,
+	})
 }
 
 // middleware log
@@ -90,6 +142,7 @@ func main() {
 
 	cfg := &apiConfig{
 		DB: dbQueries,
+		Platform: os.Getenv("PLATFORM"),
 	}
 
 	mux := http.NewServeMux() //creating a new mux
@@ -102,21 +155,23 @@ func main() {
 
 	mux.Handle("/app/", handler)
 
-	mux.HandleFunc("GET /healthz/", customHandler)
+	mux.HandleFunc("GET /healthz", customHandler)
 
-	mux.HandleFunc("GET /metrics/", cfg.MetricsHandler)
+	mux.HandleFunc("GET /metrics", cfg.MetricsHandler)
 
-	mux.HandleFunc("POST /reset/", cfg.ResetHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.ResetHandler)
 
 	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
+	mux.HandleFunc("POST /api/users", cfg.handleUsersCreate)
+
 	//creating the new server
 	server := &http.Server{
-		Addr:    ":80",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	log.Println("starting the server on :80")
+	log.Println("starting the server on :8080")
 
 	err = server.ListenAndServe()
 
